71976fee216fc29a0f660afb8331e308fe81d9b8
Lucas Soares <lucassoares2007@hotmail.com>
2024-02-22 09:35:36 -0300

    refactor: base project

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express'
 import { createImageService } from '../services'
 
 export const createImageController = async (req: Request, res: Response) => {
-  await createImageService(req.body)
+  await createImageService(req.user.id, req.file)
   return res.status(201).json({})
 }
